70ef3ce4bcb20dde3b86debcb2554226c09bf6d6
AcostaDDev <acostadavdevelopment>
2024-04-06 13:40:31 +0200

    Echo GET Data (I forgot to add basic.py)

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,8 +5,8 @@ import requests
 #endpoint = "http://localhost:8000/" # http://127.0.0.1:8000/
 endpoint = "http://localhost:8000/api/" # http://127.0.0.1:8000/
 
-get_response = requests.get(endpoint, json={"query":"Hello World"}) #API -> HTTP Request  || REST APIs -> Web APIs
+get_response = requests.get(endpoint, params={"product_id": 123}) #API -> HTTP Request  || REST APIs -> Web APIs
 
 #print(get_response.text) # print raw text response
-print(get_response.json()['message']) # print json response
+print(get_response.json()) # print json response
 #print(get_response.status_code)
