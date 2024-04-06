f76a557974107b80fb796294f6ea512ea6cb00f7
AcostaDDev <acostadavdevelopment>
2024-04-06 13:02:00 +0200

    Prueba de api con Django

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,12 @@
 import requests
 
 # endpoint = "http://httpbin.org/status/200"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/" # http://127.0.0.1:8000/
 
 get_response = requests.get(endpoint, json={"query":"Hello World"}) #API -> HTTP Request  || REST APIs -> Web APIs
 
-# print(get_response.text) # print raw text response
+print(get_response.text) # print raw text response
 
 # print(get_response.json()) # print json response
 
