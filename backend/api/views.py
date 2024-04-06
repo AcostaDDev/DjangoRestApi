9f3d59f779ed1fc9617f94c78ffcb24f7a06a6da
AcostaDDev <acostadavdevelopment>
2024-04-06 13:38:39 +0200

    Echo GET Data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,21 @@
+import json
 from django.http import JsonResponse
 
+
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message":"Hi there this is your Django API response"})
+
+    print(request.GET)
+    #print(request.POST)
+    body = request.body #byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) # gets JSON data and converts it to a Python dict object
+    except:
+        pass
+    print(data)
+
+    data['params'] = dict(request.GET)
+    print(request.headers)
+    data['headers'] = (dict(request.headers))
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
